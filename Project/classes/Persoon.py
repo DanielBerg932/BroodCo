467c86b12a8a885d36aaac2e31b6e44f38341226
Daniel <danielberger932@gmail.com>
2022-07-22 17:33:33 +0200

    dictionary fixed

--- Project/classes/Persoon.py
+++ Project/classes/Persoon.py
@@ -15,35 +15,33 @@ class Client(Courier):
         self.count = 0
         self.count = count
         self.name = 'empty'
-        self.breadDict = {
-            'bType': 'null',
-            'datetime': 'null',
-            'machineNr': -1,
-            'guid': 'null'}
 
     def addToStorage(self, breadItem, machine):
+        breadDict = {}
         if breadItem != None:
-            self.breadDict['bType'] = breadItem._type()
-            self.breadDict['datetime'] = str(
+            breadDict['bType'] = breadItem._type()
+            breadDict['datetime'] = str(
                 datetime.now().strftime("%m-%d at %H:%M"))
             # ensures the dict item is actually overwrtitten because its unique
-            self.breadDict['guid'] = uuid.uuid4()
-            self.breadDict['machineNr'] = machine
-            self.storage.append(self.breadDict)
+            breadDict['guid'] = uuid.uuid4()
+            breadDict['machineNr'] = machine
+            self.storage.append(breadDict)
 
     def __str__(self):
         dictArr = []
+        defaultString = type(self).__name__ + ':' + \
+            str(self.count) + \
+            '----------------------------------------' + '\n\t'
+        breadString = defaultString
         for d in self.storage:
             for value in d.values():
                 dictArr.append(value)
-            defaultString = type(self).__name__ + ':' + \
-                str(self.count) + \
-                '----------------------------------------' + '\n\t'
+
             btype = str(dictArr[0])
             date = str(dictArr[1])
-            machine = str(dictArr[2])
-            guid = str(dictArr[3])
-            breadString = defaultString+'Bread: '+btype+'\n\t'+'purchase date: ' + \
-                date+'\n\t'+'machine Nr: '+machine+'\n\t'+'ID:'+guid+'\n'
-        dictArr = []
+            guid = str(dictArr[2])
+            machine = str(dictArr[3])
+            breadString += 'Bread: '+btype+'\n\t'+'purchase date: ' + \
+                date+'\n\t'+'machine Nr: '+machine+'\n\t'+'ID:'+guid+'\n\t\n\t'
+            dictArr = []
         return breadString
